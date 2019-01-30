2b5f1898272840319dd315eea26eec22b172216d
prakashmaharjan <prakashmaharjan20@gmail.com>
2019-01-30 16:11:51 +0545

    project added

---
+++ app/src/main/java/com/example/prakashmaharjan/recyclerviewdemokotlin/MainAdapter.kt
@@ -0,0 +1,42 @@
+package com.example.prakashmaharjan.recyclerviewdemokotlin
+
+import android.support.v7.widget.RecyclerView
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import kotlinx.android.synthetic.main.row_layout.view.*
+
+class MainAdapter: RecyclerView.Adapter<CustomViewHolder>(){
+
+
+    var listArray = listOf<String>("Hello", "world", "Hi", "People", "Hey")
+    // number of items or rows
+    override fun getItemCount(): Int {
+        //return  4
+        return  listArray.size
+
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): CustomViewHolder {
+        //TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        val layoutInflater = LayoutInflater.from(parent?.context)
+        val cellForRow = layoutInflater.inflate(R.layout.row_layout, parent, false)
+        return  CustomViewHolder(cellForRow)
+    }
+
+
+    override fun onBindViewHolder(holder: CustomViewHolder, position: Int) {
+        //TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+
+       // holder?.view.TitleTextViewID.text = "12345"
+
+        val title = listArray.get(position)
+
+        holder?.view.TitleTextViewID.text = title
+    }
+}
+
+
+class CustomViewHolder(val view: View): RecyclerView.ViewHolder(view){
+
+}
