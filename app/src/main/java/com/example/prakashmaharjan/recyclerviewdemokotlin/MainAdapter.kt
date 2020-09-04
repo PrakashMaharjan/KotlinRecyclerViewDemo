70b39f84cde760b0abf08395699594f6c1d51351
prakashmaharjan <prakashmaharjan20@gmail.com>
2020-09-04 21:16:51 +0545

    formating

--- app/src/main/java/com/example/prakashmaharjan/recyclerviewdemokotlin/MainAdapter.kt
+++ app/src/main/java/com/example/prakashmaharjan/recyclerviewdemokotlin/MainAdapter.kt
@@ -6,14 +6,33 @@ import android.view.View
 import android.view.ViewGroup
 import kotlinx.android.synthetic.main.row_layout.view.*
 
-class MainAdapter: RecyclerView.Adapter<CustomViewHolder>(){
+class MainAdapter : RecyclerView.Adapter<CustomViewHolder>() {
 
-    var listArray =listOf<String>("Android 1.0", "Petit Four", "Cupcake", "Donut", "Eclair", "Froyo", "Gingerbread", "Honeycomb", "Ice Cream Sandwich", "Jelly Bean", "Kitkat", "Lollipop","Marshmallow", "Nougat", "Oreo", "Pie")
+    var listArray = listOf<String>(
+        "Android 1.0",
+        "Petit Four",
+        "Cupcake",
+        "Donut",
+        "Eclair",
+        "Froyo",
+        "Gingerbread",
+        "Honeycomb",
+        "Ice Cream Sandwich",
+        "Jelly Bean",
+        "Kitkat",
+        "Lollipop",
+        "Marshmallow",
+        "Nougat",
+        "Oreo",
+        "Pie",
+        "Android 10",
+        "Android 11"
+    )
 
     // number of items or rows
     override fun getItemCount(): Int {
         //return  4
-        return  listArray.size
+        return listArray.size
 
     }
 
@@ -21,14 +40,14 @@ class MainAdapter: RecyclerView.Adapter<CustomViewHolder>(){
         //TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
         val layoutInflater = LayoutInflater.from(parent.context)
         val cellForRow = layoutInflater.inflate(R.layout.row_layout, parent, false)
-        return  CustomViewHolder(cellForRow)
+        return CustomViewHolder(cellForRow)
     }
 
 
     override fun onBindViewHolder(holder: CustomViewHolder, position: Int) {
         //TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
 
-       // holder?.view.TitleTextViewID.text = "12345"
+        // holder?.view.TitleTextViewID.text = "12345"
 
         val title = listArray.get(position)
 
@@ -37,6 +56,6 @@ class MainAdapter: RecyclerView.Adapter<CustomViewHolder>(){
 }
 
 
-class CustomViewHolder(val view: View): RecyclerView.ViewHolder(view){
+class CustomViewHolder(val view: View) : RecyclerView.ViewHolder(view) {
 
 }
